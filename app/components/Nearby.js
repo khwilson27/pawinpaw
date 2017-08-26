b16ff392d945dd936a3fe66d5a7a318bb6b5721b
nhuyp <ypham87@gmail.com>
2017-08-26 11:00:51 -0700

    Some additional screens

---
+++ app/components/Nearby.js
@@ -0,0 +1,56 @@
+// Include React as a dependency
+var React = require("react");
+
+// Include the Helper (for the saved recall)
+// var helpers = require("../utils/helpers");
+
+// Create the Main component
+var Nearby = React.createClass({
+
+  // Our render method. Utilizing a few helper methods to keep this logic clean
+  render: function () {
+    return (
+      <div className="mainContainer">
+        {/* Navigation bar */}
+        <div className="row">
+          <div className="col-sm-12 col-xs-12">
+            <div className="navBar">navBar here</div>
+          </div>
+        </div>
+        
+        <div className="container">
+        {/* Login fields */}
+        <div class="row">
+            <div className="col-sm-12 col-xs-12">
+                <Image source={{uri: "http://via.placeholder.com/350x350"}}/>
+            </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-8 col-xs-8">
+            <form>
+              <button type="submit" className="btn btn-default">Dislike</button>
+              <br></br>
+              <button type="submit" className="btn btn-default">Like</button>
+            </form>
+          </div>
+        </div>
+        {/* Footer */}
+        <div className="row">
+          <div className="col-sm-12 col-xs-12">
+            <footer>
+              <hr />
+              <p className="pull-right">
+                <i className="fa fa-github" aria-hidden="true"></i>
+                Paw in Paw &copy; 2017
+            </p>
+            </footer>
+          </div>
+        </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+// Export the module back to the route
+module.exports = Nearby;

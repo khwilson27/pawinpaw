f3a64a27a840fb227fd423f44d2d0f68ed1bb1a0
Kevin Wilson <kevinhn27@gmail.com>
2017-08-28 18:41:22 -0700

    working react routes for login/reg, working google login

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,41 +1,20 @@
-// Include React as a dependency
-var React = require("react");
-// Including the Link component from React Router to navigate within our application without full page reloads
-// https://github.com/ReactTraining/react-router/blob/master/docs/API.md#link
+import React from "react";
+import ReactDOM from "react-dom"; 
+import helpers from "../utils/helpers.js";
+
 var Link = require("react-router").Link;
 
-//Main Component Style
-
-const mainComponentStyle = {
-  width: "414px",
-  height: "736px",
-  margin: "auto",
-  color: "#009191"
-} 
-
-const headerStyle = {
-  width:  "414px",
-  height: "150px",
-
-}
-
-const middleStyle = {
-  width:  "414px",
-  height: "600px",
-  color: "#009191"
-}
 
 // Create the Main component
-var Main = React.createClass({
+class Main extends React.Component{
 
-  render: function() {
+  render() {
 
     return (
       // We can only render a single div. So we need to group everything inside of this main-container one
-      <div className="main-container" style = {mainComponentStyle}>
-        <div className="">
-          {/* Navbar */}
-          <nav className="navbar navbar-default" role="navigation" style = {headerStyle}>
+      <div className="main-container">
+          Navbar
+          <nav className="navbar navbar-default" role="navigation">
             <div className="container-fluid">
               <div className="navbar-header">
                 <button
@@ -55,19 +34,12 @@ var Main = React.createClass({
               <div className="collapse navbar-collapse navbar-ex1-collapse">
                 <ul className="nav navbar-nav navbar-right">
                   {/* Using <Link> in place of <a> and "to" in place of "href" */}
-                  <li><Link to="/search">Login</Link></li>
-                  <li><Link to="/saved">Register</Link></li>
+                  <li><Link to="/Login">Login</Link></li>
+                  <li><Link to="/Registration">Register</Link></li>
                 </ul>
               </div>
             </div>
           </nav>
-
-          {/* Jumbotron */}
-          <div className="jumbotron" style={middleStyle}>
-     
-          </div>
-
-
           {/* Here we will deploy the sub components (Search or Saved */}
           {/* These sub-components are getting passed as this.props.children */}
           {this.props.children}
@@ -79,11 +51,11 @@ var Main = React.createClass({
               Paw in Paw &copy; 2017
             </p>
           </footer>
-        </div>
+        
       </div>
     );
   }
-});
+};
 
 // Export the module back to the route
-module.exports = Main;
+export default Main;

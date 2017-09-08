ef2a381d63d0bf56db4aab61928bef5530e596a7
Reina Natasha <reinanatasha@Reinas-MacBook-Pro.local>
2017-09-07 20:15:01 -0700

    rendered profile pic

--- app/components/Match.js
+++ app/components/Match.js
@@ -1,6 +1,7 @@
 // Include React as a dependency
 import React from "react";
 import helpers from "../utils/helpers.js";
+import Iframe from "react-iframe";
 
 var Link = require("react-router").Link;
 var router = require("react-router");
@@ -39,24 +40,37 @@ class Match extends React.Component {
 
         return (
             <div>
-                <h1>No matches yet...</h1>
+                    <div>
+                         <h1>No matches yet...</h1>
+                     </div>
+
+            
+                 <Iframe url="http://thebark.com/sites/default/files/styles/thumbnail_medium/public/content/article/thumb/dog_pitbull_breed_shelter.jpg?itok=mOxjTbIU"
+                                width="150px"
+                                height="150px"
+                                display="initial"
+                                position="relative"
+                                allowFullScreen/>
+                                
             </div>
         )
     }
 
     renderMatches() {
         // console.log("full");
+        
+                
+                    this.state.matchProfiles.map((currentValue, index) => {
+                
+                        // console.log(currentValue);
+                        return( <p>
+                        
+                        {currentValue.name}</p>)}
 
-        return this.state.matchProfiles.map((currentValue, index) => {
-            // console.log(currentValue);
-            return <p>{currentValue.name}</p>
+                    )
+                      
 
-        })
-
-
-
-
-    }
+                }
 
     // Our render method. Utilizing a few helper methods to keep this logic clean
     render() {
